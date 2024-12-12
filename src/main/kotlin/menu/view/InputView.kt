926feda44cbd99dbd262dec250be966b7b8f43a4
HamBeomJoon <hbj0802@naver.com>
2024-12-12 22:55:45 +0900

    refactor(InputView): 코치 이름 입력 로직 수정

--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -1,18 +1,30 @@
 package menu.view
 
 import camp.nextstep.edu.missionutils.Console
+import menu.model.Coach
+import menu.model.Menu
 import menu.utils.Constants
 import menu.utils.Validator
 
 object InputView {
-    fun getCoachNames(): List<String> {
+    fun getCoachNames(): List<Coach> {
         while (true) {
             println(Constants.INPUT_COACH_NAME)
             val input = Console.readLine()
             try {
                 Validator.validateCoachCount(input)
                 Validator.validateCoachName(input)
-                return input.split(",")
+                val coachList = mutableListOf<Coach>()
+                for (name in input.split(",")) {
+                    coachList.add(Coach(name = name, cantEat = null))
+                }
+                return coachList
+            } catch (e: IllegalArgumentException) {
+                println(e.message)
+            }
+        }
+    }
+
             } catch (e: IllegalArgumentException) {
                 println(e.message)
             }
