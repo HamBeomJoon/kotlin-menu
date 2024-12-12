b58f42f96601b23e929b1418a358d54917b7f0ee
HamBeomJoon <hbj0802@naver.com>
2024-12-12 21:26:11 +0900

    feat(MenuController): inputView, OutputView 함수 호출

---
+++ src/main/kotlin/menu/controller/MenuController.kt
@@ -0,0 +1,15 @@
+package menu.controller
+
+import menu.view.InputView
+import menu.view.OutputView
+
+class MenuController {
+    private val inputView = InputView
+    private val outputView = OutputView
+
+    fun start() {
+        outputView.printStartMessage()
+        inputView.getCoachNames()
+
+    }
+}
